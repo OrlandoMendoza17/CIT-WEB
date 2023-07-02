39f584cb5d40b85f2d8764bc884ce00ac2c99cfa
Luis Mario <51382475+Luism96xd@users.noreply.github.com>
2023-07-01 22:32:30 -0400

    Update cyberseguridad.tsx post body

--- src/pages/blog/cyberseguridad.tsx
+++ src/pages/blog/cyberseguridad.tsx
@@ -17,24 +17,26 @@ const cyberseguridad = () => {
       <main>
         <PostHero {...post} />
         <PostContent>
-          <p><strong>Imperdible</strong> 👍🏻🔊 Conferencia Encuentro con la Ciberseguridad BC ¿Quieres optimizar tus conocimientos respecto a la seguridad y protección de datos personales, equipos y softwares?</p>
+          <p>A medida que nuestra sociedad se vuelve cada vez más digital, la seguridad informática se vuelve esencial.</p>
+          <p>Sin embargo, los ataques cibernéticos son cada vez más frecuentes y sofisticados, así que proteger nuestros datos personales se ha vuelto más desafiante que nunca.</p>
+          <p>¿Sabías que un solo virus puede poner en peligro toda nuestra información? </p>
+          <p>Por esta razón, el equipo de Proyectos Institucionales CIT te invita al "Encuentro con la Ciberseguridad" ¿Te gustaría ampliar tus conocimientos sobre a la seguridad informática y la protección de tus datos personales? ¡Esta ponencia es para ti!</p>
           <ul>
             <li>📍 Día viernes 👉 20 de  julio a las ⏰ 1:00 P.M.</li>
-            <li>📍 Aula Magistral 2B</li>
+            <li>📍 Virtual: Vía Google Meet</li>
             <li>📍 Certificado de Participación🧑🏽‍🎓</li>
-            <li>📍 Valor: 3$ dólares pagaderos en Bolívares.</li>
+            <li>📍 Entrada Libre.</li>
           </ul>
           
           <p><strong>¡Inscríbete a través del siguiente <a href="https://forms.gle/b4kFQYXteRmyKPTL7">Formulario!</a>  ✅👈🏻🦉</strong></p>
           
-          <img width={600} src="https://i.imgur.com/MTolrPw.jpg" alt="" />
+          <img width={600} src="https://i.imgur.com/MTolrPw.jpg" alt="banner publicitario" />
+
+          <p>Además, al inscribirte estarás participando por el acceso a una masterclass sobre business intelligence e inteligencia artificial en donde aprenderás a elaborar Dashboards, con la herramienta Google Data Studio y ChatGPT.</p>
           
-          <p><strong>Datos de Pago:</strong></p>
-          <p>Cédula de Identidad: 29.912.020</p>
-          <p>Número de Teléfono: 0424-4217139</p>
-          <p>Banco: (0105) Mercantil</p>
-          
-          
+          <p>¡Inscríbete en nuestra conferencia sobre ciberseguridad y aprende a proteger tus datos personales!</p>
+          <p>Podrás unirte a la conferencia a través del Google Meet el 20 de julio a la 1:00p.m.</p>
+          <p>¡No te lo pierdas!</p>
         </PostContent>
       </main>
       <Footer />
